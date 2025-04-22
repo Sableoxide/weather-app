ae1dfe4d74044acf667404b4bca73b706f6cb5c3
Sableoxide <gokofrank432@gmail.com>
2025-04-22 23:25:05 +0300

    correct folder

---
+++ next-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <button className="btn btn-primary"> hello </button>
+      <button className="btn btn-primary">Hello World!</button>
+    </div>
+  );
+}
