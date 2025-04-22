8d90489b1f11da3f574d231e49fcc610930d9506
Sableoxide <gokofrank432@gmail.com>
2025-04-22 23:55:42 +0300

    todays forecast api call

--- laravel_api/app/Http/Controllers/Weather.php
+++ laravel_api/app/Http/Controllers/Weather.php
@@ -3,10 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class Weather extends Controller
-{
-    public function weatherToday() {
-        return "testing weather today";
+{   
+    public function weatherToday(Request $request) {
+        $api_key = config("services.open_weather_map.key");
+        $longitude = $request->query('lon');
+        $latitude = $request->query('lat');
+        $url = "api.openweathermap.org/data/2.5/forecast/daily?lat=$latitude&lon=$longitude&cnt=1&appid=$api_key";
+
+        try {
+            $response = Http::timeout(3)->get($url);
+            if ($response->successful()) {
+                if (empty(json_decode($response->getBody(), true))) {
+                    return response()->json(['message' => 'forecast not found'],404);
+                } else {
+                    return [
+                        "message" => "API request success",
+                        "status" => $response->status(),
+                        "body" => $response->body()
+                    ];
+                }
+            } else {
+                return [
+                    "error" => "API request not SUCCESSFUL",
+                    "status" => $response->status(),
+                    "body" => $response->body()
+                ];
+            }
+
+        } catch (\Exception $e) {
+            return [
+                "error" => "API call failed",
+                "message" => $e->getMessage()
+            ];
+        }
     }
 }
--- laravel_api/routes/web.php
+++ laravel_api/routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Geocoding;
 use App\Http\Controllers\Weather;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/today', [Weather::class, 'weatherToday']);
+Route::get('/today-forecast', [Weather::class, 'weatherToday']);
 
 Route::get('/location/{city}', [Geocoding::class, 'locationCoordinates']);
